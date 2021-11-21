f4400334916aedfb31cce2022285bbf68fa750da
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-11-21 23:03:30 +0100

    Cycle icon elements

--- 1-foods/script.js
+++ 1-foods/script.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log("Hello World!");
+const section1FirstElement = document.querySelector('.section1-icons i');
+setInterval(() => {
+    const section1VisibleElement = document.querySelector('.section1-icons i:not(.hidden)');
+    const section1NextElement = section1VisibleElement.nextElementSibling;
+    section1NextElement ? section1NextElement.classList.remove('hidden') : section1FirstElement.classList.remove('hidden');
+    section1VisibleElement.classList.add('hidden');
+}, 4000);
--- 1-foods/script.ts
+++ 1-foods/script.ts
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const section1FirstElement: Element = document.querySelector('.section1-icons i')!;
+setInterval(()=>{
+    const section1VisibleElement: Element = document.querySelector('.section1-icons i:not(.hidden)')!;
+    const section1NextElement: any = section1VisibleElement.nextElementSibling;
+    section1NextElement ? section1NextElement.classList.remove('hidden') : section1FirstElement.classList.remove('hidden');
+    section1VisibleElement.classList.add('hidden');
+},
+4000);
